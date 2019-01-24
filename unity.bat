0560cb0b8aa56ccb4da3409d43fe1ba378443f46
达尼 <hushiyu@shangyoo.com>
2019-01-24 17:26:49 +0800

    feat ：bat file

--- unity.bat
+++ unity.bat
@@ -2,9 +2,9 @@
  
 echo 正在生成APK文件...  
  
-"C:\Program Files\Unity\Editor\Unity.exe" -projectPath C:\Users\hushiyu\Desktop\TestProject\PracticeProject -quit -batchmode -executeMethod BuildTest.Test -logFile build.log
+REM "C:\Program Files\Unity\Editor\Unity.exe" -projectPath C:\Users\hushiyu\Desktop\TestProject\PracticeProject -quit -batchmode -executeMethod BuildTest.Test -logFile build.log
  
-REM %1 -projectPath %2 -quit -batchmode -executeMethod BuildTest.Test -logFile build.log
+%1 -projectPath %2 -quit -batchmode -executeMethod BuildTest.Test -logFile build.log
  
 echo APK文件生成完毕!  
 pause 
