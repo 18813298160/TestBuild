b8bc8e5d069c6e8a971a6d06e0f811dfd6507a94
达尼 <hushiyu@shangyoo.com>
2019-01-24 18:23:58 +0800

    feat : Test

--- unity.bat
+++ unity.bat
@@ -4,7 +4,7 @@ echo generating apk file...
  
 REM "C:\Program Files\Unity\Editor\Unity.exe" -projectPath C:\Users\hushiyu\Desktop\TestProject\PracticeProject -quit -batchmode -executeMethod BuildTest.Test -logFile build.log
  
-%1 -projectPath D:\Program Files (x86)\Jenkins\workspace\TestBuild\PracticeProject -quit -batchmode -executeMethod BuildTest.Test -logFile build.log
+%1 -projectPath -projectPath C:\Users\hushiyu\Desktop\TestProject\PracticeProject -quit -batchmode -executeMethod BuildTest.Test -logFile build.log
  
 echo generate apk done!  
 pause 
