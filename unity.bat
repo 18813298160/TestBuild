28279a7cac3f0efd9b1ee056896bc71a3f54e9a2
达尼 <hushiyu@shangyoo.com>
2019-01-24 17:49:49 +0800

    feat：editor 脚本

--- unity.bat
+++ unity.bat
@@ -1,10 +1,10 @@
 @echo off  
  
-echo 正在生成APK文件...  
+echo generating apk file...  
  
 REM "C:\Program Files\Unity\Editor\Unity.exe" -projectPath C:\Users\hushiyu\Desktop\TestProject\PracticeProject -quit -batchmode -executeMethod BuildTest.Test -logFile build.log
  
 %1 -projectPath %2 -quit -batchmode -executeMethod BuildTest.Test -logFile build.log
  
-echo APK文件生成完毕!  
+echo generate apk done!  
 pause 
