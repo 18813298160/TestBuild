6fafc80eec390e3e2c3f65b7948ab31d8a36b94b
达尼 <hushiyu@shangyoo.com>
2019-01-24 19:09:01 +0800

    feat ：Revert

--- unity.bat
+++ unity.bat
@@ -4,7 +4,7 @@ echo generating apk file...
  
 REM "C:\Program Files\Unity\Editor\Unity.exe" -projectPath C:\Users\hushiyu\Desktop\TestProject\PracticeProject -quit -batchmode -executeMethod BuildTest.Test -logFile build.log
  
-%1 -projectPath C:\Users\hushiyu\Desktop\TestProject\PracticeProject -quit -batchmode -executeMethod BuildTest.Test -logFile build.log
+"C:\Program Files\Unity\Editor\Unity.exe" -projectPath %2 -quit -batchmode -executeMethod BuildTest.Test -logFile build.log
  
 echo generate apk done!  
 pause 
